1a49259794fbabc25f812164aa7db06228649c8e
曹智博 <czb2013nj@163.com>
2018-12-29 14:30:40 +0800

    新工程

---
+++ boot-crm/src/com/itheima/crm/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package com.itheima.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 客户信息请求处理
+ * @author 曹智博
+ *
+ */
+@Controller
+@RequestMapping("customer")
+public class CustomerController {
+
+	@RequestMapping("list")
+	public String list(){
+		return "customer";
+	}
+	
+}
---
+++ boot-crm/src/com/itheima/crm/mapper/BaseDictMapper.java
@@ -0,0 +1,32 @@
+package com.itheima.crm.mapper;
+
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.itheima.crm.pojo.BaseDict;
+import com.itheima.crm.pojo.BaseDictExample;
+
+public interface BaseDictMapper {
+    int countByExample(BaseDictExample example);
+
+    int deleteByExample(BaseDictExample example);
+
+    int deleteByPrimaryKey(String dictId);
+
+    int insert(BaseDict record);
+
+    int insertSelective(BaseDict record);
+
+    List<BaseDict> selectByExample(BaseDictExample example);
+
+    BaseDict selectByPrimaryKey(String dictId);
+
+    int updateByExampleSelective(@Param("record") BaseDict record, @Param("example") BaseDictExample example);
+
+    int updateByExample(@Param("record") BaseDict record, @Param("example") BaseDictExample example);
+
+    int updateByPrimaryKeySelective(BaseDict record);
+
+    int updateByPrimaryKey(BaseDict record);
+}
